d18e4807f1643569a966a89ee1e993afd43e7373
Melnyk Vladyislav <you@example.com>
2024-04-18 10:10:19 +0300

    new commit

--- handlers/conversation_handlers/first_conversation_handler.py
+++ handlers/conversation_handlers/first_conversation_handler.py
@@ -4,7 +4,7 @@ from telegram.ext import ConversationHandler, CommandHandler, ContextTypes, Mess
 
 from handlers.base_handler import BaseHandler
 
-GENDER, PHOTO, AGE = range(3)
+GENDER, PHOTO, AGE, BUTTON = range(4)
 
 
 class FirstConversationHandler(BaseHandler):
@@ -28,7 +28,7 @@ class FirstConversationHandler(BaseHandler):
             [KeyboardButton('Boy'), KeyboardButton('Girl')],
         ]
         reply_text = ReplyKeyboardMarkup(keyboard)
-        await update.message.reply_text(f'Hello {update.effective_user.first_name}! Are you a Boy or a Girl?',reply_markup=reply_text)
+        await update.message.reply_text(f'Hello {update.effective_user.first_name}! Are you a Boy or a Girl?', reply_markup=reply_text)
 
         return GENDER
 
@@ -40,30 +40,42 @@ class FirstConversationHandler(BaseHandler):
 
     @staticmethod
     async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'You are a {update.message.text}. Share your photo, please!',reply_markup=ReplyKeyboardRemove())
+        gender = update.message.text
+        context.user_data['gender'] = gender
+        await update.message.reply_text(f'You are a {gender}. Share your photo, please!', reply_markup=ReplyKeyboardRemove())
 
         return PHOTO
 
     @staticmethod
     async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await update.message.reply_text(f'Thank you for your photo!')
-        keyboard = [
-            [
-                InlineKeyboardButton("1", callback_data="1"),
-                InlineKeyboardButton("2", callback_data="2"),
-            ],
-            [InlineKeyboardButton("3", callback_data="3")],
-        ]
+        await update.message.reply_text(f"Thank you for your photo! What's your age?")
+
+        keyboard = []
+        number = 1
+
+        for i in range(10):
+            row = []
+
+            for j in range(5):
+                row.append(InlineKeyboardButton(f"{number}", callback_data=f"{number}"))
+                number += 1
+
+            keyboard.append(row)
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text("Choose your age:", reply_markup=reply_markup)
 
         return AGE
 
     @staticmethod
-    async def age(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    async def age(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """Parses the CallbackQuery and updates the message text."""
         query = update.callback_query
         await query.answer()
 
-        await query.edit_message_text(text=f"Your age is: {query.data}")
-        return ConversationHandler.END
+        age = query.data
+        context.user_data['age'] = age
+
+        gender = context.user_data.get('gender', 'unknown')
+        await query.edit_message_text(text=f"Ви {gender}. Ваш вік - {age} років.")
+
+        return AGE
